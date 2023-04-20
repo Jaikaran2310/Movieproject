23d002e78007b110a1e661c7865a9ef6a9bb5025
Jaikaran <jaikaransingh2310@gmail.com>
2023-04-19 21:52:33 -0700

    11 commit

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,42 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 import contact from "./contact.png";
 
 const Contact = () => {
+  const [Name, setname] = useState("");
+  const [Email, setemail] = useState("");
+  const [Subject, setsubject] = useState("");
+  const [Description, setdescription] = useState("");
+  const [Data, setdata] = useState([]);
+  const adddata = () => {
+    setdata([...Data,{Name,Email,Subject,Description}])
+  }
+  console.log(adddata)
+  
+
   return (
     <div>
       <section className="contact">
-      <div className="contact-form">
-        <h1>
-          Contact <span>Us</span>
-        </h1>
+        <div className="contact-form">
+          <h1>
+            Contact <span>Us</span>
+          </h1>
 
-        <p>Any questions or remarks? Just write us a message!</p>
-        <form action="">
-          <input type="" placeholder="Your Name" required />
-          <input
-            type="email"
-            className="email"
-            id=""
-            placeholder="E-mail"
-            required
-          />
-          <input type="" placeholder="Write a Subject" required />
-          <textarea
-            className=""
-            id=""
-            cols="30"
-            rows="10"
-            placeholder="Your Message"
-            required
-          ></textarea>
-          <input type="Submit"  value="submit" className="btn" />
-        </form>
+          <p>Any questions or remarks? Just write us a message!</p>
+          <form action="">
+            <input
+              type=""
+              value={Name}
+              onChange={(event) => setname(event.target.value)}
+              placeholder="Your Name"
+              required
+            />
+            <input
+              type="email"
+              value={Email}
+              onChange={(event) => setemail(event.target.value)}
+              className="email"
+              id=""
+              placeholder="E-mail"
+              required
+            />
+            <input type=""
+            value={Subject}
+            onChange={(event) => setsubject(event.target.value)}
+             placeholder="Write a Subject" required />
+            <textarea
+              className=""
+              value={Description}
+              onChange={(event) => setdescription(event.target.value)}
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Your Message"
+              required
+            ></textarea>
+            <input type="Submit" value="submit" className="btn" />
+          </form>
         </div>
         <div className="contact-img">
-          <img src={contact} alt="pic"/>
+          <img src={contact} alt="pic" />
         </div>
-      
       </section>
     </div>
   );
